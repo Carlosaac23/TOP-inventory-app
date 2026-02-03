2deb912fa1286031427145455ab67c97447b611a
Carlos <carlosaac232001@gmail.com>
2026-02-02 22:11:06 -0500

    feat: - set express static middleware to serve public directory - set express urlencoded middleware to parse form data - import dirname and fileURL from path module - set views directory and view engine to ejs

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
+import path from 'node:path';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 import express from 'express';
+
+import { trainsRoutes } from './routes/trainRoutes.js';
 
 const app = express();
 const PORT = 8080;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const assetsPath = path.join(__dirname, 'public');
 
-app.get('/', (req, res) => res.send('Holaaaaaaaaaa'));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.static(assetsPath));
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/', trainsRoutes);
 
 app.listen(PORT, error => {
   if (error) {
