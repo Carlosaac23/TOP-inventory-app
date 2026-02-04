b6ac52e6792fa55c394796883dcf3b59966f62da
Carlos <carlosaac232001@gmail.com>
2026-02-03 22:32:39 -0500

    feat: create getTrainByIdController to handle getTrainById query function

--- controllers/trainControllers.js
+++ controllers/trainControllers.js
@@ -1,6 +1,12 @@
-import { getAllTrains } from '../db/trainQueries.js';
+import { getAllTrains, getTrainById } from '../db/trainQueries.js';
 
 export async function getAllTrainsController(req, res) {
   const trains = await getAllTrains();
   res.render('trains/index', { trains });
+}
+
+export async function getTrainByIdController(req, res) {
+  const { trainID } = req.params;
+  const train = await getTrainById(trainID);
+  res.render('trains/infoTrain', { train });
 }
