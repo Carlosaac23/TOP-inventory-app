269652323d4c9791b1aaf25036bedfb24db06e80
Carlos <carlosaac232001@gmail.com>
2026-02-04 11:25:11 -0500

    feat: - get scales, brands and wagon_categories from db to render them dynamically - add res.status and res.send error in catch for all functions

--- controllers/wagonControllers.js
+++ controllers/wagonControllers.js
@@ -1,12 +1,21 @@
-import { getAllWagons, getWagonById } from '../db/wagonQueries.js';
+import { getAllScales, getAllBrands } from '../db/generalQueries.js';
+import {
+  getAllWagons,
+  getWagonById,
+  getAllWagonCategories,
+} from '../db/wagonQueries.js';
 
 export async function getAllWagonsController(req, res) {
   try {
     const wagons = await getAllWagons();
+    const scales = await getAllScales();
+    const brands = await getAllBrands();
+    const categories = await getAllWagonCategories();
 
-    res.render('wagons/index', { wagons });
+    res.render('wagons/index', { wagons, scales, brands, categories });
   } catch (error) {
     console.error(error);
+    res.status(500).send('Error loading wagons');
   }
 }
 
@@ -18,5 +27,6 @@ export async function getWagonByIdController(req, res) {
     res.render('wagons/infoWagon', { wagon });
   } catch (error) {
     console.error(error);
+    res.status(500).send('Error getting wagon');
   }
 }
