d8d0fb5052f1bf69836336b23725f61e97db3477
Carlos <carlosaac232001@gmail.com>
2026-02-04 11:23:35 -0500

    feat: - get scales, brands and train_categories from db to render them dynamically - add res.status and res.send error in catch for all functions

--- controllers/trainControllers.js
+++ controllers/trainControllers.js
@@ -1,12 +1,21 @@
-import { getAllTrains, getTrainById } from '../db/trainQueries.js';
+import { getAllScales, getAllBrands } from '../db/generalQueries.js';
+import {
+  getAllTrains,
+  getTrainById,
+  getAllTrainCategories,
+} from '../db/trainQueries.js';
 
 export async function getAllTrainsController(req, res) {
   try {
     const trains = await getAllTrains();
+    const scales = await getAllScales();
+    const brands = await getAllBrands();
+    const categories = await getAllTrainCategories();
 
-    res.render('trains/index', { trains });
+    res.render('trains/index', { trains, scales, brands, categories });
   } catch (error) {
     console.error(error);
+    res.status(500).send('Error loading trains');
   }
 }
 
@@ -18,5 +27,6 @@ export async function getTrainByIdController(req, res) {
     res.render('trains/infoTrain', { train });
   } catch (error) {
     console.error(error);
+    res.status(500).send('Error getting train');
   }
 }
