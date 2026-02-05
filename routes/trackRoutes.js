632922096c64ab6983c20558200f5178abc6f498
Carlos <carlosaac232001@gmail.com>
2026-02-05 14:16:04 -0500

    feat: add routes to render editForm and send new data

--- routes/trackRoutes.js
+++ routes/trackRoutes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import {
   getAllTracksController,
   getTrackByIdController,
+  getUpdateFormController,
+  putUpdateFormController,
 } from '../controllers/trackControllers.js';
 
 export const trackRoutes = Router();
 
 trackRoutes.get('/', getAllTracksController);
+trackRoutes
+  .route('/:trackID/update')
+  .get(getUpdateFormController)
+  .put(putUpdateFormController);
 trackRoutes.get('/:trackID', getTrackByIdController);
