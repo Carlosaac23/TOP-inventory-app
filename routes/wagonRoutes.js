ddfd42ce1b004d60fc0a182ea81051eb71252ac6
Carlos <carlosaac232001@gmail.com>
2026-02-05 12:12:59 -0500

    feat: create route to send new wagon changes

--- routes/wagonRoutes.js
+++ routes/wagonRoutes.js
@@ -3,9 +3,15 @@ import { Router } from 'express';
 import {
   getAllWagonsController,
   getWagonByIdController,
+  getUpdateFormController,
+  putUpdateFormController,
 } from '../controllers/wagonControllers.js';
 
 export const wagonRoutes = Router();
 
 wagonRoutes.get('/', getAllWagonsController);
+wagonRoutes
+  .route('/:wagonID/update')
+  .get(getUpdateFormController)
+  .put(putUpdateFormController);
 wagonRoutes.get('/:wagonID', getWagonByIdController);
