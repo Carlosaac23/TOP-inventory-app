f9897ae59a3b1d9b8d239f2c685616fe54d0df57
Carlos <carlosaac232001@gmail.com>
2026-02-05 12:12:39 -0500

    feat: create functions render the editForm and send those changes

--- controllers/wagonControllers.js
+++ controllers/wagonControllers.js
@@ -2,6 +2,7 @@ import { getAllScales, getAllBrands } from '../db/generalQueries.js';
 import {
   getAllWagons,
   getWagonById,
+  updateWagonById,
   getAllWagonCategories,
 } from '../db/wagonQueries.js';
 
@@ -10,6 +11,7 @@ export async function getAllWagonsController(req, res) {
 
   try {
     const wagons = await getAllWagons({ category, scale, brand });
+    console.log('Wagons: ', wagons);
     const scales = await getAllScales();
     const brands = await getAllBrands();
     const categories = await getAllWagonCategories();
@@ -36,5 +38,49 @@ export async function getWagonByIdController(req, res) {
   } catch (error) {
     console.error(error);
     res.status(500).send('Error getting wagon');
+  }
+}
+
+export async function getUpdateFormController(req, res) {
+  try {
+    const { wagonID } = req.params;
+    const wagon = await getWagonById(wagonID);
+
+    if (!wagon) {
+      return res.status(404).send('Wagon not found');
+    }
+
+    res.render('forms/updateForm', {
+      title: 'Wagon',
+      item: wagon,
+      path: 'wagons',
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error getting wagon');
+  }
+}
+
+export async function putUpdateFormController(req, res) {
+  try {
+    const { wagonID } = req.params;
+    const { model, model_id, description, price, stock_quantity } = req.body;
+
+    const updatedWagon = await updateWagonById(wagonID, {
+      model,
+      model_id,
+      description,
+      price: Number(price),
+      stock_quantity: Number(stock_quantity),
+    });
+
+    if (!updatedWagon) {
+      return res.status(404).send('Wagon not found');
+    }
+
+    return res.redirect(`/wagons/${wagonID}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error updating wagon');
   }
 }
