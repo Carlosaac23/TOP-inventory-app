76c8bee192bae0a977c145c2cb7e3338705c3196
Carlos <carlosaac232001@gmail.com>
2026-02-05 10:02:09 -0500

    feat: get filter from req.query and based on that, filter the trains

--- controllers/trainControllers.js
+++ controllers/trainControllers.js
@@ -6,13 +6,21 @@ import {
 } from '../db/trainQueries.js';
 
 export async function getAllTrainsController(req, res) {
+  const { category, scale, brand } = req.query;
+
   try {
-    const trains = await getAllTrains();
+    const trains = await getAllTrains({ category, scale, brand });
     const scales = await getAllScales();
     const brands = await getAllBrands();
     const categories = await getAllTrainCategories();
 
-    res.render('trains/index', { trains, scales, brands, categories });
+    res.render('trains/index', {
+      trains,
+      scales,
+      brands,
+      categories,
+      filters: { category, scale, brand },
+    });
   } catch (error) {
     console.error(error);
     res.status(500).send('Error loading trains');
