498d962e23bfaf31c120761f674023e9ef8d1a2c
Carlos <carlosaac232001@gmail.com>
2026-02-05 11:30:43 -0500

    feat: add routes to get edit form and save the edit

--- routes/trainRoutes.js
+++ routes/trainRoutes.js
@@ -3,9 +3,12 @@ import { Router } from 'express';
 import {
   getAllTrainsController,
   getTrainByIdController,
+  getUpdateForm,
+  putUpdateForm,
 } from '../controllers/trainControllers.js';
 
 export const trainRoutes = Router();
 
 trainRoutes.get('/', getAllTrainsController);
+trainRoutes.route('/:trainID/update').get(getUpdateForm).put(putUpdateForm);
 trainRoutes.get('/:trainID', getTrainByIdController);
