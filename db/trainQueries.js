e7a68791fbc90a3aa186d19bd760fe7a619fa3a8
Carlos <carlosaac232001@gmail.com>
2026-02-05 10:01:39 -0500

    feat: add logic to add many filters and return that trains

--- db/trainQueries.js
+++ db/trainQueries.js
@@ -1,7 +1,28 @@
 import { pool } from './pool.js';
 
-export async function getAllTrains() {
-  const { rows } = await pool.query('SELECT * FROM trains');
+export async function getAllTrains({ category, scale, brand }) {
+  const values = [];
+  const where = [];
+
+  if (category) {
+    values.push(category);
+    where.push(`category_id = $${values.length}`);
+  }
+  if (scale) {
+    values.push(scale);
+    where.push(`scale_id = $${values.length}`);
+  }
+  if (brand) {
+    values.push(brand);
+    where.push(`brand_id = $${values.length}`);
+  }
+
+  let sql = 'SELECT * FROM trains';
+  if (where.length) {
+    sql += ` WHERE ${where.join(' AND ')}`;
+  }
+
+  const { rows } = await pool.query(sql, values);
   return rows;
 }
 
